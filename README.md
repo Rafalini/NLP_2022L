87b0441c5abec414ed5e022e5ebbd0d7c495a864
rbosko <prajmik@gmail.com>
2022-04-19 21:20:45 +0200

    master - datasets download

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # NLP_2022L
+
+
+Project structure:
+
+root/
+    / Data /
+    |      /sourceData        - unmodified data from internet
+    |      /preprecessedData  - preprocessed data
+    |
+    / docs /                  - documentation
+    |
+    / models /                - models, weights, configuration
---
+++ data/getDataSets.py
@@ -0,0 +1,69 @@
+import os
+import tarfile
+import zipfile
+
+import requests
+
+
+def downloadGeniaDataset():
+    urls = [
+        "http://www.nactem.ac.uk/GENIA/current/GENIA-corpus/Relation/GENIA_relation_annotation_training_data.tar.gz",
+        "http://www.nactem.ac.uk/GENIA/current/GENIA-corpus/Relation/GENIA_relation_annotation_development_data.tar.gz",
+        "http://www.nactem.ac.uk/GENIA/current/GENIA-corpus/Relation/GENIA_relation_annotation_test_data.tar.gz"]
+
+    print('Downloading GENIA dataset, urls: '+str(len(urls)))
+    geniaPath = os.path.join(os.getcwd() + '/sourceData/genia')
+
+    if not os.path.exists(geniaPath):
+        os.mkdir(geniaPath)
+
+    for url in urls:
+        datasetName = url.split('/')[-1]
+        response = requests.get(url)
+        open(os.path.join(geniaPath, datasetName), "wb").write(response.content)
+        archive = tarfile.open(os.path.join(geniaPath, datasetName))
+        archive.extractall(geniaPath)
+        os.remove(os.path.join(geniaPath, datasetName))
+
+
+def downloadBlurbDataset():
+    urls = ["https://biocreative.bioinformatics.udel.edu/media/store/files/2016/CDR_Data.zip",
+            "https://biocreative.bioinformatics.udel.edu/media/store/files/2015/BC5CDR-converter-0.0.2.zip"]
+
+    print('Downloading BLURB dataset, urls: '+str(len(urls)))
+    blurbPath = os.path.join(os.getcwd() + '/sourceData/blurb')
+
+    if not os.path.exists(blurbPath):
+        os.mkdir(blurbPath)
+
+    for url in urls:
+        datasetName = url.split('/')[-1]
+        response = requests.get(url)
+        open(os.path.join(blurbPath, datasetName), "wb").write(response.content)
+        archive = zipfile.ZipFile(os.path.join(blurbPath, datasetName))
+        archive.extractall(blurbPath)
+
+
+def downloadConllDataset():
+    urls = ["https://data.deepai.org/conll2003.zip"]
+
+    print('Downloading CoNLL 2003 dataset, urls: '+str(len(urls)))
+    conllPath = os.path.join(os.getcwd() + '/sourceData/conll2003')
+
+    if not os.path.exists(conllPath):
+        os.mkdir(conllPath)
+
+    for url in urls:
+        datasetName = url.split('/')[-1]
+        response = requests.get(url)
+        open(os.path.join(conllPath, datasetName), "wb").write(response.content)
+        archive = zipfile.ZipFile(os.path.join(conllPath, datasetName))
+        archive.extractall(conllPath)
+
+    os.remove(os.path.join(conllPath, 'conll2003.zip'))
+
+
+if __name__ == "__main__":
+    downloadGeniaDataset()
+    downloadBlurbDataset()
+    downloadConllDataset()
