312545bce4ef05b2812f6c3e6f5341cbcd11b6ea
rbosko <prajmik@gmail.com>
2022-05-18 00:16:11 +0200

    master - initial model & cleanup

---
+++ consts.py
@@ -0,0 +1,19 @@
+import os
+from simpletransformers.classification import ClassificationArgs
+
+DATA_DIR = 'data'
+CONL_TRAIN_DATASET = os.path.join(DATA_DIR, 'conll2003', 'train.txt')
+CONL_TEST_DATASET = os.path.join(DATA_DIR, 'conll2003', 'test.txt')
+CONL_VALID_DATASET = os.path.join(DATA_DIR, 'conll2003', 'valid.txt')
+
+INIT_TRAIN_SIZE = 100
+MAX_TRAIN_SIZE = 1000
+STEP = 100
+
+#####################################################################
+BERT_MODEL_TYPE = 'roberta'  # bert, roberta, xlm, ...
+BERT_MODEL_NAME = 'roberta-base'
+BERT_ARGS = ClassificationArgs(
+    model_type=BERT_MODEL_TYPE,
+    # overwrite_output_dir=True
+)
---
+++ models/bert.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from simpletransformers.classification import ClassificationModel
+
+from models.abstract_classifier import TextEntityClassifier
+
+
+class BertWrapper(TextEntityClassifier):
+    def __init__(self, model: ClassificationModel):
+        self.model: ClassificationModel = model
+
+    def train(self, data):
+        self.model.train_model(
+            train_df=data,
+            show_running_loss=False
+        )
+
+    def predict(self, inputs):
+        predictions, raw_outputs = self.model.predict(inputs)
+        return predictions
+
+    def prepare_training_data(self, raw_data: pd.DataFrame):
+        data = raw_data.copy()
+        # 'text' and 'labels' columns already present, no need to do anything
+        data = data.astype({'text': str, 'labels': str})
+        return data[['text', 'labels']]
