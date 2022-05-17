9a3b6566bffe83bbb4d16bbe92197f6c4830abdd
rbosko <prajmik@gmail.com>
2022-05-18 00:19:51 +0200

    master - additional utils

---
+++ utils.py
@@ -0,0 +1,30 @@
+import torch
+import numpy as np
+import os
+import random
+import warnings
+
+
+def seed_torch(seed=2137):
+    #  taken from: https://github.com/pytorch/pytorch/issues/7068
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def turn_off_stupid_warnings():
+    # ugly, but simpletransfomers.T5 throws some stupid
+    # deprecation warnings if everything is done the way
+    # the official tutorial says: https://simpletransformers.ai/docs/t5-model/
+    warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+def prepare_environment():
+    turn_off_stupid_warnings()
+    seed_torch()
+
