7c55c8e458a2545c5eda747bf8f2bd43146297f3
rbosko <prajmik@gmail.com>
2022-05-22 18:23:24 +0200

    master - data read 02

--- consts.py
+++ consts.py
@@ -1,12 +1,12 @@
 import os
 from simpletransformers.classification import ClassificationArgs
 
-DATA_DIR = 'data'
+DATA_DIR = './data/sourceData'
 CONL_TRAIN_DATASET = os.path.join(DATA_DIR, 'conll2003', 'train.txt')
 CONL_TEST_DATASET = os.path.join(DATA_DIR, 'conll2003', 'test.txt')
 CONL_VALID_DATASET = os.path.join(DATA_DIR, 'conll2003', 'valid.txt')
 
-INIT_TRAIN_SIZE = 100
+INIT_TRAIN_SIZE = 500
 MAX_TRAIN_SIZE = 1000
 STEP = 100
 
@@ -17,3 +17,4 @@ BERT_ARGS = ClassificationArgs(
     model_type=BERT_MODEL_TYPE,
     # overwrite_output_dir=True
 )
+BERT_OUTPUT = 'bert-output'
