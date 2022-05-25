e1fe8ebdc215a9072e708daa8b2c4117521f0aa7
rbosko <prajmik@gmail.com>
2022-05-25 18:24:25 +0200

    master - new csv model

--- data/preprocessData.py
+++ data/preprocessData.py
@@ -24,20 +24,9 @@ def getDataset(path):
 def conllCleanup(input, output):
     df = getDataset(input)
     df = df.drop(columns=['document', 'label', 'sentence'])
-    df = initial_preprocess(df)
     print(df.head)
-    df = df[['text', 'pos']]
-    df.to_csv(output)
-
-
-def initial_preprocess(df: pd.DataFrame):
-    df['text'] = df['text'].astype('string')
-    # make everything uppercase
-    df['text'] = df['text'].str.upper()
-    # remove punctuation
-    for i, text in enumerate(df['pos']):
-        df.loc[i, 'text'] = re.sub(r'[^\w\s#]', '', text)  # DO NOT REMOVE HASH
-    return df
+    # df = df[['text', 'pos']]
+    # df.to_csv(output)
 
 
 def simpleCSV(input, output, encoding):
@@ -52,6 +41,13 @@ def simpleCSV(input, output, encoding):
     if encoding == 'BIO':
         df['label1'] = IOB2BIO(df['label1'])
     df['label1'] = pd.factorize(df['label1'])[0]
+    df.to_csv(output)
+
+
+def newCSV(input, output):
+    df = pd.read_csv(filepath_or_buffer=input, sep=' ')
+    df.rename(columns={'-DOCSTART-': 'text', '-X-': 'partofSpeach', '-X-.1': 'syntactic', 'O': 'entityTag'}, inplace=True)
+    print(df.head)
     df.to_csv(output)
 
 
@@ -87,10 +83,13 @@ def IOB2BIO(values):
 
 if __name__ == '__main__':
 #option 1
-    # conllCleanup(input_file, output_file)
+    # newCSV('./sourceData/conll2003/test.txt', './preprocessedData/conll2003/preprocessed_test.csv')
+    # newCSV('./sourceData/conll2003/train.txt', './preprocessedData/conll2003/preprocessed_train.csv')
+    # newCSV('./sourceData/conll2003/valid.txt', './preprocessedData/conll2003/preprocessed_valid.csv')
 #option2
     for encoding in ['IOB', 'IO', 'BIO']:
         for data_type in test_or_train:
+            print('Processing: '+encoding+' '+data_type)
             input_file = f'./sourceData/conll2003/{data_type}.txt'
             output_file = f'./preprocessedData/conll2003/preprocessed_{data_type}_{encoding}.csv'
             simpleCSV(input_file, output_file, encoding)
